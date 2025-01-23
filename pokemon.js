34b2e4a9681761f40d5cb59383feb796f070303d
Parthib <parthib.chakraborty.dev@gmail.com>
2025-01-23 12:37:41 +0530

    Add files via upload

---
+++ pokemon.js
@@ -0,0 +1,17 @@
+async function fetchData() {
+    try{
+        const pokemonName = document.getElementById('pokemonName').value.toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+        if(!response.ok){
+            throw new Error(response.statusText);
+        }
+        const data = await response.json();
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
+    }
+    catch(error){
+        console.log(error);
+    }
+}
